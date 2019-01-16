99fb0c3df2d8f8bdff4d860bec919e0ded068ea1
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-16 09:16:39 -0800

    Initialization of files

---
+++ DeckCollection/DeckCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeckCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DeckCollection/DeckCollectionUnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DeckCollectionUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
